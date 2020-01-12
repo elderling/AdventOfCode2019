58946aa472b745ee6d9b3dcb4d2f95cd25462872
Jonathan William Taylor <elderling@gmail.com>
2020-01-12 14:50:31 -0600

    Introduced separate IntCodeComputer Class for Advent 2019 Day 5a

--- src/com/perlartifex/AdventDay05a.java
+++ src/com/perlartifex/AdventDay05a.java
@@ -7,56 +7,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class AdventDay05a {
-    public static int HALT = 99;
-    public static int ADD = 1;
-    public static int MULTIPLY = 2;
 
     public static void main( String[] args) {
-        //int output = runProgram(12, 2);
-        int arbitraryOutput = 19690720;
-
-        int finalNoun = 0;
-        int finalVerb = 0;
-        // this could be made more efficient by exiting the loops when we find our answer but the number of cases is 100 * 100
-        for( int noun = 0; noun < 100; noun++) {
-            for( int verb = 0; verb < 100; verb++) {
-                if( runProgram(noun, verb) == arbitraryOutput ) {
-                    finalNoun = noun;
-                    finalVerb = verb;
-                }
-            }
-        }
-
-        System.out.println("Final Noun: " + finalNoun);
-        System.out.println("Final Verb: " + finalVerb);
-    }
-
-    public static int runProgram( int noun, int verb) {
         int[] originalRam = readInput();
-        int[] ram = originalRam.clone();
-        int programCounter = 0;
-
-        ram[1] = noun;
-        ram[2] = verb;
-        while( ram[programCounter] != HALT ) {
-            int opcode = ram[programCounter];
-            int registerA = ram[programCounter + 1];
-            int registerB = ram[programCounter + 2];
-            int registerC = ram[programCounter + 3];
-
-            if(opcode == ADD) {
-                System.out.println("Add");
-                ram[registerC] = ram[registerA] + ram[registerB];
-            }
-            else if(opcode == MULTIPLY) {
-                System.out.println("Multiply");
-                ram[registerC] = ram[registerA] * ram[registerB];
-            }
-            programCounter += 4;
-        }
-        System.out.println("HALT");
-
-        return ram[0];
     }
 
     public static int[] readInput() {
---
+++ src/com/perlartifex/IntCodeComputer.java
@@ -0,0 +1,60 @@
+package com.perlartifex;
+
+import java.util.Map;
+
+public class IntCodeComputer {
+    private int[] ram;
+
+    private static int ADD = 1;
+    private static int MULTIPLY = 2;
+    private static int INPUT = 3;
+    private static int OUTPUT = 4;
+    private static int HALT = 99;
+
+    // Instruction, Number of Parameters
+    private static Map<Integer,Integer> instructionLength = Map.of(
+            ADD, 3,
+            MULTIPLY, 3,
+            INPUT, 1,
+            OUTPUT, 1,
+            HALT, 0
+    );
+
+    IntCodeComputer(int[] ram) {
+        this.ram = ram.clone(); // we don't want to mutate the input
+    }
+
+    public int runProgram() {
+        int programCounter = 0;
+        int instruction = ram[programCounter];
+
+        String instructionString = String.format("%05d", instruction);
+
+        int opcode = Integer.parseInt(instructionString.substring(3,4));
+        int param1mode = Integer.parseInt(instructionString.substring(2,2));
+        int param2mode = Integer.parseInt(instructionString.substring(1,1));
+        int param3mode = Integer.parseInt(instructionString.substring(0,0));
+        while (opcode != HALT) {
+            var numberOfParameters = this.instructionLength.get(opcode);
+
+            if (numberOfParameters == 1 ) {
+                var parameter1 = ram[programCounter+1];
+            }
+            else if (numberOfParameters == 3 ) {
+                var parameter1 = ram[programCounter+1];
+                var parameter2 = ram[programCounter+2];
+                var parameter3 = ram[programCounter+3];
+            }
+            programCounter += numberOfParameters + 1;
+        }
+
+        return 0;
+    }
+    /*
+    Instruction, Number of Parameters
+    ADD, 3
+    Multiply, 3
+    INPUT, 1
+    OUTPUT, 1
+     */
+}
